3c1b76f5e4b804d58125236d36b9380f44131de5
catsirup <taehyung0116@gmail.com>
2020-02-05 11:32:16 +0900

    버그 수정 및 readme 수정

--- project.py
+++ project.py
@@ -37,7 +37,7 @@ driver.implicitly_wait(3)
 
 page_index = 1
 # 크롤링 시작 일자
-f = '2019.02.05'
+f = '2020.01.01'
 # 크롤링 종료 일자
 t = '2020.02.05'
 period_txt = "&period=" + f + ".%7C" + t + "."
@@ -46,14 +46,12 @@ _sort_kind = sort_kind(2)
 f = open("result/url_list.txt", 'w')
 page_url = []
 while True:
+    driver.implicitly_wait(3)
     driver.get('https://kin.naver.com/search/list.nhn?query=' + get_keyword(keyword) + period_txt + "&section=kin" + "&page=" + str(page_index) + "&sort=" + _sort_kind)
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
     tags = soup.find_all('a', class_="_nclicks:kin.txt _searchListTitleAnchor")
-    if len(tags) == 0:
-        break
-
     for tag in tags:
         url = str(tag).split(' ')[3]
         url = url.replace('href=', "")
@@ -62,7 +60,19 @@ while True:
         page_url.append(url)
         f.write(url + "\n")
 
-    page_index += 1
+    post_number = driver.find_element_by_class_name('number').text
+    post_number = str(post_number).replace("(", "")
+    post_number = str(post_number).replace(")", "")
+    
+    current_number = post_number.split('/')[0].split('-')[1]
+    current_number = current_number.replace(',', '')
+    total_number = post_number.split('/')[1]
+    total_number = total_number.replace(',', '')
+
+    if int(current_number) == int(total_number):
+        break
+    else:
+        page_index += 1
 
 date = str(datetime.now()).replace('.', '_')
 date = date.replace(' ', '_')
@@ -77,6 +87,7 @@ for j in range(1, 4):
     sheet.cell(row=1, column=j).fill = PatternFill(start_color='808080', end_color='808080', fill_type='solid')
 
 for i in page_url:
+    driver.implicitly_wait(3)
     driver.get(i)
 
     title = driver.find_element_by_class_name('title').text
